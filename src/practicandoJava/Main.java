a34a8879f56b6a9e9f26fa6bf32012764a566601
JesusHerreraAl <jherreraalvarado@educa.madrid.org>
2026-09-14 22:42:33 +0200

    Modificando un poco el codigo, introduce un menu y una suma

--- src/practicandoJava/Main.java
+++ src/practicandoJava/Main.java
@@ -1,10 +1,52 @@
 package practicandoJava;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo este merge sera uno de los primeros");
+		Scanner entrada = new Scanner(System.in);
+		int opcion = 0;
+
+		String menu = "1- Suma \n2- Resta\n0- Salir";
+
+		do {
+			System.out.println(menu);
+			System.out.println("Dame una opcion");
+			opcion = entrada.nextInt();
+
+			switch (opcion) {
+			case 1:
+				double a, b;
+				System.out.println("Eligio suma");
+				System.out.println("Dame el numero");
+				a = entrada.nextDouble();
+				System.out.println("Dame otro numero");
+				b = entrada.nextDouble();
+				System.out.println("La suma es: " + suma(a, b));
+
+				break;
+			case 2:
+				break;
+			case 0:
+				System.out.println("Adios");
+				break;
+			default:
+				System.out.println("Opcion no valida");
+				break;
+			
+			}
+
+		} while (opcion != 0);
+
+	}
+
+	public static double suma(double a, double b) {
+		double total = 0;
+		total = a + b;
+		return total;
 
 	}
 
