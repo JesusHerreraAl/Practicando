56a5cb3f7654737a09b3d59fc66d2d8e4f94d04a
JesusHerreraAl <jherreraalvarado@educa.madrid.org>
2026-09-14 22:12:02 +0200

    Practicando commit

---
+++ src/practicandoJava/Main.java
@@ -0,0 +1,11 @@
+package practicandoJava;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo este merge sera uno de los primeros");
+
+	}
+
+}
